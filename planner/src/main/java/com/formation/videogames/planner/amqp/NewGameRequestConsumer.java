c93385992434ec18fcc056e374eea4c8cd0e181f
cristoflop <cristofer.lc.99@gmail.com>
2021-05-21 15:35:04 +0200

    Added initial config for consumer and producer

---
+++ planner/src/main/java/com/formation/videogames/planner/amqp/NewGameRequestConsumer.java
@@ -0,0 +1,16 @@
+package com.formation.videogames.planner.amqp;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewGameRequestConsumer {
+
+    @KafkaListener(topics = "messages")
+    public void received(ConsumerRecord<?, ?> message) {
+
+        System.out.println("Message: "+message.value());
+    }
+
+}
---
+++ planner/src/main/java/com/formation/videogames/planner/amqp/NewGameResponsePublisher.java
@@ -0,0 +1,28 @@
+package com.formation.videogames.planner.amqp;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NewGameResponsePublisher {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private int numData;
+
+    public NewGameResponsePublisher(KafkaTemplate kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void sendMessage() {
+
+        String data = "Data " + numData++;
+
+        System.out.println("publishToQueue: '" + data + "'");
+
+        kafkaTemplate.send("messages", data);
+    }
+
+}
