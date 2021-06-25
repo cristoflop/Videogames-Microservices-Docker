dbb4c4a13c03a50ab5ddc694d2b8a55585240a01
cristoflop <cristofer.lc.99@gmail.com>
2021-06-25 13:00:51 +0200

    Add wait for connections in microservices

--- planner/src/main/java/com/formation/videogames/planner/Application.java
+++ planner/src/main/java/com/formation/videogames/planner/Application.java
@@ -2,14 +2,22 @@ package com.formation.videogames.planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.policy.AlwaysRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		RetryTemplate template = new RetryTemplate();
+		AlwaysRetryPolicy policy = new AlwaysRetryPolicy();
+		template.setRetryPolicy(policy);
+		template.execute(context -> {
+			SpringApplication.run(Application.class, args);
+			return true;
+		});
+	}
 
 }
--- server/src/main/java/com/formation/videogames/server/Application.java
+++ server/src/main/java/com/formation/videogames/server/Application.java
@@ -2,14 +2,22 @@ package com.formation.videogames.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.policy.AlwaysRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class Application {
-
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		RetryTemplate template = new RetryTemplate();
+		AlwaysRetryPolicy policy = new AlwaysRetryPolicy();
+		template.setRetryPolicy(policy);
+		template.execute(context -> {
+			SpringApplication.run(Application.class, args);
+			return true;
+		});
 	}
-
+	
 }
