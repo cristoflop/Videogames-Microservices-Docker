c6c30a7925b334d79122d5f3dbd7eb3785037979
cristoflop <cristofer.lc.99@gmail.com>
2021-06-26 15:29:54 +0200

    Mark MessageServiceImpl as service

--- server/src/main/java/com/formation/videogames/server/services/MessageServiceImpl.java
+++ server/src/main/java/com/formation/videogames/server/services/MessageServiceImpl.java
@@ -5,18 +5,22 @@ import java.util.Date;
 import com.formation.videogames.server.data.MessageRepository;
 import com.formation.videogames.server.domain.Event;
 import com.formation.videogames.server.domain.Message;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class MessageServiceImpl implements MessageService {
 
-	private MessageRepository messageRepository;
+    private MessageRepository messageRepository;
 
-	public MessageServiceImpl(MessageRepository messageRepository) {
-		this.messageRepository = messageRepository;
-	}
+    public MessageServiceImpl(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
-	public String saveCreation(String gameId) {
-		Message message = new Message(gameId, Event.CREATION, new Date("<YYYY-mm-ddTHH:MM:ss>"));
-		return this.messageRepository.save(message).getId();
-	}
+    public String saveCreation(String gameId) {
+        Message message = new Message(gameId, Event.CREATION, new Date("<YYYY-mm-ddTHH:MM:ss>"));
+        return this.messageRepository.save(message).getId();
+    }
 
 }
