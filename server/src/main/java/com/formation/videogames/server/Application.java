181b11b59ae7fa5f12c743d080b26b72c1869f20
cristoflop <cristofer.lc.99@gmail.com>
2021-06-28 09:03:54 +0200

    Add docker compose for dev

--- server/src/main/java/com/formation/videogames/server/Application.java
+++ server/src/main/java/com/formation/videogames/server/Application.java
@@ -2,8 +2,6 @@ package com.formation.videogames.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.policy.AlwaysRetryPolicy;
-import org.springframework.retry.support.RetryTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
