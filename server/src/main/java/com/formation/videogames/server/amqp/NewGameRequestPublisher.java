1e14977fb659b51baca88505aa812327a16dea32
cristoflop <cristofer.lc.99@gmail.com>
2021-06-28 13:05:21 +0200

    Add @Component

--- server/src/main/java/com/formation/videogames/server/amqp/NewGameRequestPublisher.java
+++ server/src/main/java/com/formation/videogames/server/amqp/NewGameRequestPublisher.java
@@ -1,16 +1,18 @@
 package com.formation.videogames.server.amqp;
 
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.formation.videogames.server.services.dto.NewGameDto;
 
+@Component
 public class NewGameRequestPublisher {
 
 	private KafkaTemplate<String, String> kafkaTemplate;
 
-	private final String NEW_GAME_REQUEST_TOPIC = "new-game-request-topic";
+	private static final String NEW_GAME_REQUEST_TOPIC = "new-game-request-topic";
 
 	private ObjectMapper om;
 
