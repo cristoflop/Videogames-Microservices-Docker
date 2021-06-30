9122f71a940c1b4e0d391280bf56f8375f82e8bb
cristoflop <cristofer.lc.99@gmail.com>
2021-06-30 17:18:38 +0200

    Add swagger in planner

--- planner/src/main/java/com/formation/videogames/planner/controllers/GameRestController.java
+++ planner/src/main/java/com/formation/videogames/planner/controllers/GameRestController.java
@@ -19,6 +19,15 @@ import com.formation.videogames.planner.services.GameService;
 import com.formation.videogames.planner.services.dto.GameDto;
 import com.formation.videogames.planner.services.dto.NewGameDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Tag(name = "Game rest controller")
 @RestController
 @RequestMapping("/api")
 public class GameRestController {
@@ -29,6 +38,9 @@ public class GameRestController {
 		this.gameService = gameService;
 	}
 
+	@Operation(summary = "Devuelve una lista con todos los juegos de la BD o busca por nombre")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Libros encontrados", content = {
+			@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = GameDto.class))) }) })
 	@GetMapping("/games")
 	public ResponseEntity<List<GameDto>> findAll(@RequestParam(required = false) String name) {
 		List<GameDto> games;
@@ -41,18 +53,25 @@ public class GameRestController {
 		return ResponseEntity.status(HttpStatus.OK).body(games);
 	}
 
+	@Operation(summary = "Guarda un juego en BD")
+	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "Juego guardado en BD") })
 	@PostMapping("/games")
 	public ResponseEntity<String> save(@RequestBody NewGameDto game) throws JsonProcessingException {
 		String id = this.gameService.save(game);
 		return ResponseEntity.status(HttpStatus.CREATED).body(id);
 	}
 
+	@Operation(summary = "Devuelve un juego si existe")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Juego encontrado", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = GameDto.class)) }) })
 	@GetMapping("/games/{id}")
 	public ResponseEntity<GameDto> findOne(@PathVariable("id") String id) {
 		GameDto game = this.gameService.findOne(id);
 		return ResponseEntity.status(HttpStatus.OK).body(game);
 	}
 
+	@Operation(summary = "Devuelve un juego si existe")
+	@ApiResponses(value = { @ApiResponse(responseCode = "204", description = "Juego borrado") })
 	@DeleteMapping("/games/{id}")
 	public ResponseEntity<Void> delete(@PathVariable("id") String id) {
 		this.gameService.delete(id);
