c8521f9c3e187c4d611afc0ca8095c9bc557dd3a
cristoflop <cristofer.lc.99@gmail.com>
2021-06-30 17:37:52 +0200

    Add swagger in server

--- server/src/main/java/com/formation/videogames/server/controller/MessageRestController.java
+++ server/src/main/java/com/formation/videogames/server/controller/MessageRestController.java
@@ -16,6 +16,15 @@ import com.formation.videogames.server.services.MessageService;
 import com.formation.videogames.server.services.dto.MessageDto;
 import com.formation.videogames.server.services.dto.NewGameDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Meesages rest controller")
 @RestController
 @RequestMapping("/api")
 public class MessageRestController {
@@ -29,12 +38,17 @@ public class MessageRestController {
 		this.newGameRequestPublisher = newGameRequestPublisher;
 	}
 
+	@Operation(summary = "Publica un mensaje de creacion de un juego en un topic")
+	@ApiResponses(value = { @ApiResponse(responseCode = "202", description = "Envia un mensaje al topic") })
 	@PostMapping("/games")
 	public ResponseEntity<Void> save(@RequestBody NewGameDto game) throws JsonProcessingException {
 		this.newGameRequestPublisher.sendMessage(game);
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
 	}
 
+	@Operation(summary = "Devuelve una lista con todos los mensajes en la BD")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Mensajes", content = {
+			@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = MessageDto.class))) }) })
 	@GetMapping("/messages")
 	public ResponseEntity<List<MessageDto>> findAll() {
 		List<MessageDto> messages = this.messageService.findAll();
